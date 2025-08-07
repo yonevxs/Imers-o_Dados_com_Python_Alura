316dca4e9bdcccc477768de6fda2ec4ae8bee4a6
unknown <lucashnevessousa@gmail.com>
2025-08-06 23:11:04 -0300

    Primeiros passos com python

---
+++ Primeiros Passos com Python/primeirosPassos.py
@@ -0,0 +1,28 @@
+nome = 'Lucas'
+cor = 'roxo'
+print('Olá, me chamo ' + nome + ', minha cor favorita é ' + cor)
+
+saldo = 5000
+print(saldo)
+
+saldo += 2000
+print(saldo)
+
+#Outra forma de utilizar variaveis em print
+print('Saldo na sua conta: R$', saldo)
+
+saldo -= 500
+print('Saldo na sua conta: R$', saldo)
+
+saldo *= 2
+print('Saldo na sua conta: R$', saldo)
+
+#Importando códigos em python
+import this
+
+#Condicional If em Python
+idade = 19
+if idade >= 18:
+    print('Você tem', idade , 'anos. Pode dirigir!')
+else:
+    print('Você tem', idade , 'anos. Não pode dirigir!')
